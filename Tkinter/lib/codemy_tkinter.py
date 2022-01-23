722fe309cd12209a484ac9712d80005b60fead67
note_m1 <marcos.r.grave@gmail.com>
2022-01-23 12:06:57 -0300

    new changes

--- Tkinter/lib/codemy_tkinter.py
+++ Tkinter/lib/codemy_tkinter.py
@@ -1,22 +1,34 @@
 # ERP - GERENCIAMENTO DE ESTOQUE
 
+# IMPORTS
 import tkinter as tk
-from tkinter import ttk
+from tkinter import DISABLED, ttk
 
+# WINDOW CONFIGS
 window = tk.Tk()  # janela
 window.title('Learning Tkinter')  # nome da janela
-window.iconbitmap('C:/Users/m1gra/Documents/Code/Python/Tkinter/resources/linux.ico') # nao aceita .png (image), apenas .ico (icon)
+window.iconbitmap('Tkinter/resources/linux.ico') # nao aceita .png (image), apenas .ico (icon)
+width = 400
+height = 250
+window.geometry(f'{width}x{height}+{int(width/4)}+{int(height/5)}')
 
-frame = ttk.Frame(window)
-frame.grid(ipadx=250, ipady=250)
+# FRAME CONFIGS
+frame = tk.Frame(window, width=width, height=height)
+frame.grid(ipadx=width, ipady=height)
 
-lbl_frame_size = ttk.Label(frame, text=f'{frame.winfo_width()} x {frame.winfo_height()}')
-lbl_frame_size.grid()
+# BUTTONS FUNCTIONS
+def click_btn_print(txt):
+    print(txt)
 
-btn1 = ttk.Button()
+# BUTTONS
+btn1 = ttk.Button(frame, text='Press to Quit', command=lambda: window.quit())
+btn2 = ttk.Button(frame, text='Shows Text', command=lambda:print('Writes a Text'))
+btn3 = ttk.Button(frame, text='Disabled Button', state=DISABLED)
 
-def click_btn1():
-    pass
+# GRID PLOT
+btn1.grid(row=0, column=0, ipadx=10, ipady=10)
+btn2.grid(row=1, column=0)
+btn3.grid(row=2, column=0, )
 
-
+# END
 window.mainloop()  # precisa disso pra janela continuar sendo atualizada. Caso contrário, ela abre e logo fecha.
