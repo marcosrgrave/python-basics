ca73c10ad576bceb3cb201b36380f5226a88a106
note_m1 <marcos.r.grave@gmail.com>
2022-01-23 12:21:34 -0300

    new changes

--- Tkinter/lib/codemy_tkinter.py
+++ Tkinter/lib/codemy_tkinter.py
@@ -3,6 +3,7 @@
 # IMPORTS
 import tkinter as tk
 from tkinter import DISABLED, ttk
+from turtle import bgcolor
 
 # WINDOW CONFIGS
 window = tk.Tk()  # janela
@@ -17,18 +18,33 @@ frame = tk.Frame(window, width=width, height=height)
 frame.grid(ipadx=width, ipady=height)
 
 # BUTTONS FUNCTIONS
-def click_btn_print(txt):
-    print(txt)
+def click_btn_label(master, txt=str, row=None, column=None):
+    lbl = ttk.Label(master, text=txt)
+    lbl.grid(row=row, column=column)
 
 # BUTTONS
-btn1 = ttk.Button(frame, text='Press to Quit', command=lambda: window.quit())
-btn2 = ttk.Button(frame, text='Shows Text', command=lambda:print('Writes a Text'))
-btn3 = ttk.Button(frame, text='Disabled Button', state=DISABLED)
+btn1 = ttk.Button(
+    frame, 
+    text='Press to Quit', 
+    command=lambda: window.quit()
+    )
+btn2 = ttk.Button(
+    frame, 
+    text='Shows Text', 
+    command=lambda:click_btn_label(
+        frame, 'Shows some text'
+        )
+    )
+btn3 = ttk.Button(
+    frame, 
+    text='Disabled Button', 
+    state=DISABLED
+    )
 
 # GRID PLOT
 btn1.grid(row=0, column=0, ipadx=10, ipady=10)
 btn2.grid(row=1, column=0)
-btn3.grid(row=2, column=0, )
+btn3.grid(row=2, column=0)
 
 # END
 window.mainloop()  # precisa disso pra janela continuar sendo atualizada. Caso contrário, ela abre e logo fecha.
